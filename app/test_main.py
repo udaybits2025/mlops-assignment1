4f61a38d288d8b0a3b088abfbedcbe5f81a03a41
Udaykumar Gajavalli <2023ac05478@wilp.bits-pilani.ac.in>
2025-08-11 20:26:26 +0530

    added line fix in pytest

--- app/test_main.py
+++ app/test_main.py
@@ -7,4 +7,5 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 404
-    # assert response.json() == {"message": "Hello World"} # commented out because a 404 will not return json
+    # assert response.json() == {"message": "Hello World"}
+    ## commented out because a 404 will not return json
